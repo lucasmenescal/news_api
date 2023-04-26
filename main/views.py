7727633712d95203766e94561faee7849889bb46
Lucas Menescal <lucasmenescalpontes@gmail.com>
2023-04-26 02:03:01 -0300

    feat: :sparkles: Adicionado o caminho get da API que retorna a lista com as news

--- main/views.py
+++ main/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
-
+import requests
 # Create your views here.
+
+
+def get(request):
+    # Endereço da News API
+    url = ('https://newsapi.org/v2/everything?q=Apple&sortBy=popularity&apiKey=c9bf327003514a86b7bc71586573ecf6')
+    response = requests.get(url)
+    news = response.json()
+    articles = news['articles']
+    descricao = []
+    titulo = []
+    imagem = []
+
+    for i in range(len(articles)):
+        article = articles[i]
+        titulo.append(article['title'])
+        descricao.append(article['description'])
+        imagem.append(article['urlToImage'])
+    
+    myList = zip(titulo, descricao, imagem)
+    context = {'myList':myList}
+
+    return render(request, 'index.html', context)
